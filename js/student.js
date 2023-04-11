8da536f4c49237b57f5ef30b4c3771bb0f5b571b
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-04-11 12:13:29 +0530

    Create getAllStudents Function

--- js/student.js
+++ js/student.js
@@ -1,3 +1,4 @@
+getAllStudents()
 function saveStudent(){
 
     let name=$('#exampleFormControlInput2').val();
@@ -17,7 +18,33 @@ function saveStudent(){
         }),
         success: function (data) {
             alert("saved")
-            // getAllStudents()
+            getAllStudents()
+        },
+        error: function (xhr, exception) {
+            alert("Error")
+        }
+    })
+}
+
+function getAllStudents(){
+
+    $.ajax({
+        method:"GET",
+        url:"http://localhost:8080/api/v1/student/getAllStudents",
+        async:true,
+        success: function (data) {
+            if (data.code==="00"){
+                $('#studentTable').empty();
+                for (let stu of data.content){
+                    let id=stu.id
+                    let name=stu.name
+                    let address=stu.address
+                    let contact=stu.contact
+
+                    var row=`<tr><td>${id}</td><td>${name}</td><td>${address}</td><td>${contact}</td></tr>`;
+                    $('#studentTable').append(row);
+                }
+            }
         },
         error: function (xhr, exception) {
             alert("Error")
